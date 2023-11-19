44e1c92ef74ccc00d5a5171dc3e483e9c6029a92
MariaDimova <150257957+MariaDimova@users.noreply.github.com>
2023-11-19 18:01:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celsius_to_Fahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double c = double.Parse(Console.ReadLine());
+            double f = 1.8 * c + 32;
+            Console.WriteLine(Math.Round(f, 2));
+
+        }
+    }
+}
